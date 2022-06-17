e0f5d005e8f68bdf47ee6cb9110155c6acd87824
Irfanul Haq <irfanulhaqdocument@gmail.com>
2022-06-17 22:07:57 +0700

    First commit, add dependencies, & utils NetworkConnectionInterceptor

---
+++ app/src/main/java/com/fanulhaq/githubuser/App.kt
@@ -0,0 +1,12 @@
+/*
+ * Copyright (c) 2022 - Irfanul Haq.
+ */
+
+package com.fanulhaq.githubuser
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+
+@HiltAndroidApp
+class App: Application()
